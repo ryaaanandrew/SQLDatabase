cd588a58698dc32908bb150e7e1387b2b71bbaa2
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-02-26 22:57:46 +0000

    added knex.destory to close connection

--- add_person.js
+++ add_person.js
@@ -19,5 +19,6 @@ const knex = require("knex")({
 
 knex('famous_people').insert({first_name: firstName, last_name: lastName, birthdate: birthdate })
     .then(function(rows) {
-        console.log('insertion complete...')
+        console.log('inserting person into database...')
+        knex.destroy();
     });
